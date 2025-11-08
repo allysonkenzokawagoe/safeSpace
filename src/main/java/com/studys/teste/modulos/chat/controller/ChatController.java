73ea2472fca2e33cc5d9547e98cb00dd0ec3a3b7
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2025-11-08 16:20:53 -0300

    Salvamento de mensagem no banco

--- src/main/java/com/studys/teste/modulos/chat/controller/ChatController.java
+++ src/main/java/com/studys/teste/modulos/chat/controller/ChatController.java
@@ -1,16 +1,23 @@
 package com.studys.teste.modulos.chat.controller; // Pacote do controller
 
 import com.studys.teste.modulos.chat.dto.ChatMessage;
+import com.studys.teste.modulos.chat.model.Mensagem;
+import com.studys.teste.modulos.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+@RequiredArgsConstructor
 @Controller
 public class ChatController {
+
+    private final ChatService chatService;
 
     @MessageMapping("/mensagemGlobal")
     @SendTo("/topic/mensagens")
     public ChatMessage enviarMensagemGlobal(ChatMessage message) {
+        chatService.save(Mensagem.of(message));
         return new ChatMessage(message.from(), message.text());
     }
 }
